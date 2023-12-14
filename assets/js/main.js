cc667e199f0468c2786f1ba2f83ac38b9d8d1374
msopiann <muhammadsopian452@gmail.com>
2023-12-14 17:58:07 +0700

    push by adri

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,3 +57,11 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// Swiper Project
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
